eba26bbe596110441e447ea6bdbf5e9aa30b337f
Sudhanshu <sudhanshurai025@gmail.com>
2023-04-27 21:35:21 +0530

    Project Complete

---
+++ server.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const mongoose = require("mongoose");
+const express = require("express");
+const session = require("cookie-session");
+const cors = require("cors");
+require("colors");
+
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config({
+	path: "./utils/config.env",
+});
+
+const userRoutes = require("./Routes/user");
+const questionRoutes = require("./Routes/questions");
+const searchRoutes = require("./Routes/search");
+const commentRoutes = require("./Routes/comments");
+const godmode = require("./Routes/godmode");
+const auth = require("./Middlewares/auth");
+const localauth = require("./Middlewares/localauth");
+const passport = require("passport");
+
+const url = process.env.MONGO;
+
+app.use(express.json());
+app.use(session({ secret: "secret" }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(url).then(() => console.log(`Connected to DB`.white.inverse));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.use("/user", userRoutes);
+	app.use("/question", questionRoutes);
+	app.use("/comment", commentRoutes);
+	app.use("/search", searchRoutes);
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve("client", "build", "index.html"))
+	);
+} else {
+	app.use("/user", userRoutes);
+	app.use("/question", questionRoutes);
+	app.use("/comment", commentRoutes);
+	app.use("/search", searchRoutes);
+	app.use("/godmode", godmode);
+}
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log(process.env.PORT);
+	console.log(`Server running at port ${process.env.PORT}`.green.inverse);
+});
